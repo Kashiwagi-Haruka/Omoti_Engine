3b807a00334f3eac14eeeca94cd8584fd512b391
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-04-17 14:02:33 +0900

    保存

--- project/Resources/shader/Object3d/PS_Shader/Object3dToon.PS.hlsl
+++ project/Resources/shader/Object3d/PS_Shader/Object3dToon.PS.hlsl
@@ -20,6 +20,8 @@ struct DirectionalLight
     float4 color;
     float3 direction;
     float intensity;
+    int shadowEnabled;
+    float3 padding;
 };
 struct PointLight
 {
@@ -28,7 +30,8 @@ struct PointLight
     float intensity;
     float radius;
     float decay;
-    float2 padding;
+    int shadowEnabled;
+    float padding;
 };
 struct PointLightCount
 {
@@ -45,7 +48,8 @@ struct SpotLight
     float decay;
     float cosAngle;
     float cosFalloffStart;
-    float2 padding;
+    int shadowEnabled;
+    float padding;
 };
 struct SpotLightCount
 {
@@ -62,7 +66,8 @@ struct AreaLight
     float height;
     float radius;
     float decay;
-    float padding;
+    int shadowEnabled;
+    float3 padding;
 };
 struct AreaLightCount
 {
@@ -99,6 +104,7 @@ static const float kToonShadowThreshold = 0.5f;
 static const float kToonShadowSoftness = 0.04f;
 static const float kToonShadowStrength = 0.35f;
 static const float kToonLightIntensityMax = 1.00f;
+
 float3 ApplyGrayscale(float3 color)
 {
     if (gMaterial.grayscaleEnabled == 0 && gCamera.fullscreenGrayscaleEnabled == 0)
@@ -133,10 +139,88 @@ float ComputeToonShadowMask(float NdotL)
 PixelShaderOutput main(Object3dVertexShaderOutput input)
 {
     PixelShaderOutput output;
-    output.color = float4(gMaterial.outlineColor.rgb, saturate(gMaterial.outlineColor.a) * gMaterial.color.a);
-    if (output.color.a <= 0.0f || gMaterial.outlineWidth <= 0.0f)
+    const float pi = 3.14159265f;
+
+    float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+
+    if (textureColor.a < 0.1f)
     {
         discard;
     }
+
+    if (gMaterial.enableLighting != 0)
+    {
+        float3 N = normalize(input.normal);
+        float lightAccumulation = 0.0f;
+
+        float3 directionalLightVector = -normalize(gDirectionalLight.direction);
+        float directionalNdotL = dot(N, directionalLightVector);
+        lightAccumulation += ComputeToonShadowMask(directionalNdotL) * gDirectionalLight.intensity;
+
+        for (uint index = 0; index < gPointLightCount.count; ++index)
+        {
+            PointLight pointLight = gPointLights[index];
+            float3 lightVector = pointLight.position - input.worldPosition;
+            float distanceToLight = length(lightVector);
+            float3 lightDirection = normalize(lightVector);
+            float attenuation = pow(saturate(1.0f - distanceToLight / pointLight.radius), pointLight.decay);
+            float NdotL = dot(N, lightDirection);
+            lightAccumulation += ComputeToonShadowMask(NdotL) * pointLight.intensity * attenuation;
+        }
+
+        for (uint index = 0; index < gSpotLightCount.count; ++index)
+        {
+            SpotLight spotLight = gSpotLights[index];
+            float3 lightVector = spotLight.position - input.worldPosition;
+            float distanceToLight = length(lightVector);
+            float3 lightDirection = normalize(lightVector);
+            float attenuation = pow(saturate(1.0f - distanceToLight / spotLight.distance), spotLight.decay);
+            float3 spotDirection = normalize(spotLight.direction);
+            float cone = dot(-lightDirection, spotDirection);
+            float falloff = saturate((cone - spotLight.cosAngle) / (spotLight.cosFalloffStart - spotLight.cosAngle));
+            float NdotL = dot(N, lightDirection);
+            lightAccumulation += ComputeToonShadowMask(NdotL) * spotLight.intensity * attenuation * falloff;
+        }
+
+        for (uint index = 0; index < gAreaLightCount.count; ++index)
+        {
+            AreaLight areaLight = gAreaLights[index];
+            float3 lightVector = areaLight.position - input.worldPosition;
+            float distanceToLight = length(lightVector);
+            float3 lightDirection = normalize(lightVector);
+            float attenuation = pow(saturate(1.0f - distanceToLight / areaLight.radius), areaLight.decay);
+            float facing = saturate(dot(normalize(areaLight.normal), -lightDirection));
+            float areaScale = areaLight.width * areaLight.height;
+            float NdotL = dot(N, lightDirection);
+            lightAccumulation += ComputeToonShadowMask(NdotL) * areaLight.intensity * attenuation * facing * areaScale;
+        }
+
+        lightAccumulation = min(lightAccumulation, kToonLightIntensityMax);
+        float toonBand = step(0.5f, lightAccumulation);
+        float shadeIntensity = lerp(kToonShadowStrength, 1.0f, toonBand);
+
+        float3 viewDirection = normalize(input.worldPosition - gCamera.worldPosition);
+        float3 reflectedDirection = reflect(viewDirection, N);
+        float2 environmentUV = float2(
+            atan2(reflectedDirection.z, reflectedDirection.x) / (2.0f * pi) + 0.5f,
+            asin(reflectedDirection.y) / pi + 0.5f);
+        float3 environmentColor = gEnvironmentTexture.Sample(gSampler, environmentUV).rgb;
+
+        float3 baseColor = gMaterial.color.rgb * textureColor.rgb;
+        float3 litColor = baseColor * shadeIntensity;
+        litColor += environmentColor * gMaterial.environmentCoefficient;
+
+        output.color.rgb = litColor;
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
+
+    output.color.rgb = ApplyGrayscale(output.color.rgb);
+    output.color.rgb = ApplySepia(output.color.rgb);
+
     return output;
 }
