d12b126d128156fed538ac3ddc43be0470f7b933
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-04 11:47:27 +0900

    a

---
+++ project/application/Object/Damage/Damage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include "Mesh/Primitive/Primitive.h"
+class Damage {
+
+	int MaxDamage_ = 99999999;
+	
+	
+
+	public:
+	void Initialize();
+	void Update();
+	void Draw();
+
+
+};
