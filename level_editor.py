a3ebb3b6250fcdff67e63b9fe72c573376ff09a2
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-11 21:19:23 +0900

    tl1

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -15,13 +16,142 @@ bl_info = {
     "category": "Object"
 }
 
-# アドオン有効化時コールバック
+
+# オペレーター：頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+
+        return {'FINISHED'}
+
+
+# オペレーター：ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+
+        return {'FINISHED'}
+
+
+# オペレーター：シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        print("シーン情報をExportします")
+
+        # シーン内の全オブジェクトについて
+        for obj in bpy.context.scene.objects:
+            print(obj.type + " - " + obj.name)
+
+            # ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            # 型は Vector, Quaternion, Vector
+            trans, rot, scale = obj.matrix_local.decompose()
+
+            # 回転を Quaternion から Euler（3軸での回転角）に変換
+            rot = rot.to_euler()
+
+            # ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+
+            # トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+
+            # 親オブジェクトの名前を表示
+            if obj.parent:
+                print("Parent:" + obj.parent.name)
+
+            # 次のオブジェクトとの区切り用の空行
+            print()
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
+
+
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    def draw(self, context):
+        self.layout.operator(
+            MYADDON_OT_stretch_vertex.bl_idname,
+            text=MYADDON_OT_stretch_vertex.bl_label
+        )
+
+        self.layout.operator(
+            MYADDON_OT_create_ico_sphere.bl_idname,
+            text=MYADDON_OT_create_ico_sphere.bl_label
+        )
+
+        self.layout.operator(
+            MYADDON_OT_export_scene.bl_idname,
+            text=MYADDON_OT_export_scene.bl_label
+        )
+
+        self.layout.separator()
+
+        self.layout.operator(
+            "wm.url_open_preset",
+            text="Manual",
+            icon='HELP'
+        )
+
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+
+# Blenderに登録するクラスリスト
+classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
+    TOPBAR_MT_my_menu,
+)
+
+
+# Add-On有効化時コールバック
 def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+
     print("レベルエディタが有効化されました。")
 
-# アドオン無効化時コールバック
+
+# Add-On無効化時コールバック
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
     print("レベルエディタが無効化されました。")
+
+
 # テスト実行用コード
 if __name__ == "__main__":
-    register()    
+    register()
