32a877beaf786161ac9ef950c817efde188eed50
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-20 16:13:46 +0900

    a

---
+++ project/application/Object/Characters/Playable/Individual/Sizuku/SizukuSpecial.cpp
@@ -0,0 +1,56 @@
+#define NOMINMAX
+#include "SizukuSpecial.h"
+#include "Object3d/Object3dCommon.h"
+
+SizukuSpecial::SizukuSpecial(){ 
+	fieldPlane_ = std::make_unique<Primitive>(); 
+
+}
+void SizukuSpecial::Initialize() { 
+	fieldPlane_->Initialize(Primitive::Plane);
+}
+void SizukuSpecial::Start() { 
+	isStarted_ = true; 
+	duration_ = 0; 
+	rainTimer_ = 0; 
+	iceRains_.clear(); 
+	fieldPlaneTransform_.translate = sizukuTransform_.translate;
+	fieldPlaneTransform_.translate.y -= sizukuHeight_ * 0.5f;
+}
+void SizukuSpecial::End() { 
+	isStarted_ = false; 
+	fieldPlaneTransform_.scale = {0.0f, 0.0f, 1.0f};
+	iceRains_.clear(); 
+}
+void SizukuSpecial::Update() { 
+	if (isStarted_) {
+		float fieldsize = fieldPlaneTransform_.scale.x;
+		fieldsize += 0.02f;
+		fieldPlaneTransform_.scale.x = std::min(fieldsize, fieldSize_);
+		fieldPlaneTransform_.scale.y = fieldPlaneTransform_.scale.x;
+		if (fieldPlaneTransform_.scale.x >= fieldSize_) {
+			duration_ += 1.0f / 60.0f;
+			rainTimer_ += 1.0f / 60.0f;
+			if (duration_ >= durationMax_) {
+				End();
+			}
+
+		}
+	}
+
+
+}
+void SizukuSpecial::Draw() {
+	if (!isStarted_) {
+		return;
+	}
+	Object3dCommon::GetInstance()->DrawCommon();
+	if (fieldPlane_) {
+		fieldPlane_->Draw();
+	}
+	//for (const auto& iceRain : iceRains_) {
+	//	if (iceRain) {
+	//		iceRain->Draw();
+	//	}
+	//}
+}
---
+++ project/application/Object/Characters/Playable/Individual/Sizuku/SizukuSpecial.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Engine/Texture/Mesh/Primitive/Primitive.h"
+#include "Engine/Texture/Mesh/Object3d/Object3d.h"
+#include "Object/Characters/Playable/Base/SpecialAttackBase.h"
+#include "Transform.h"
+#include <vector>
+#include <memory>
+class Camera;
+class SizukuSpecial : public SpecialAttackBase {
+
+	bool isStarted_ = false; // 開始フラグ
+	float duration_ = 0;     // 継続時間
+	float durationMax_ = 15; // 最大継続時間(秒)
+	float sizukuHeight_ = 0; // 雫の高さ
+	float rainInterval_ = 0.5f; // 雨の降る間隔(秒)
+	float rainTimer_ = 0;       // 雨のタイマー
+	float fieldSize_ = 5.0f;    // フィールドのサイズ
+
+	std::unique_ptr<Primitive> fieldPlane_;
+	std::vector<std::unique_ptr<Object3d>> iceRains_;
+	Transform sizukuTransform_;
+	Transform fieldPlaneTransform_;
+	Camera* camera_ = nullptr;
+
+public:
+	SizukuSpecial();
+	void Initialize();
+	void Update();
+	void Draw();
+	void Start();
+	void End();
+
+	void SetCamera(Camera* camera) { camera_ = camera; }
+	void SetSizukuTransform(Transform transform) { sizukuTransform_ = transform; }
+	void SetSizukuHeight(float height) {sizukuHeight_ = height; }
+
+
+};
