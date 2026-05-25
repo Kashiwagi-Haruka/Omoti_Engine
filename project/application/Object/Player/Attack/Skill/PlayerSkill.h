7d36f941a388dccc5f82e0bbb3aeb06b432cce67
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-05-25 23:41:56 +0900

    a

--- project/application/Object/Player/Attack/Skill/PlayerSkill.h
+++ project/application/Object/Player/Attack/Skill/PlayerSkill.h
@@ -19,12 +19,12 @@ public:
 	void StartSpecialAttack(const Transform& playerTransform, int iceCount);
 	void UpdateSpecialAttack(const Transform& playerTransform);
 	void DrawSpecialAttack();
-	bool IsSkillEnd() { return isSkillEnd; }
-	bool IsDamaging() const { return state == State::damage && !isSkillEnd; }
-	bool IsSpecialEnd() const { return isSpecialEnd_; }
-	bool IsSpecialDamaging() const { return !isSpecialEnd_; }
-	Vector3 GetDamagePosition() const { return damageTransform2_.translate; }
-	Vector3 GetDamageScale() const { return damageTransform2_.scale; }
-	const std::vector<Transform>& GetSpecialIceFlowerTransforms() const { return iceFlowerTransforms_; }
-	int GetSkillDamageId() const { return skillDamageId_; }
+	bool IsSkillEnd();
+	bool IsDamaging()const;
+	bool IsSpecialEnd()const;
+	bool IsSpecialDamaging()const;
+	Vector3 GetDamagePosition()const;
+	Vector3 GetDamageScale()const;
+	const std::vector<Transform>& GetSpecialIceFlowerTransforms()const;
+	int GetSkillDamageId()const;
 };
