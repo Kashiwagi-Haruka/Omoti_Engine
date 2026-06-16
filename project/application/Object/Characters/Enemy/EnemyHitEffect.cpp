69e33de26386857bb37e07310f71ea71cd844792
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-06-16 14:36:26 +0900

    a

--- project/application/Object/Characters/Enemy/EnemyHitEffect.cpp
+++ project/application/Object/Characters/Enemy/EnemyHitEffect.cpp
@@ -3,10 +3,15 @@
 #include "Model/ModelManager.h"
 #include <numbers>
 
+namespace {
+constexpr float kFrameTime = 1.0f / 60.0f;
+constexpr const char* kParticleTexturePath = "Resources/2d/defaultParticle.png";
+} // namespace
+
 void EnemyHitEffect::Initialize() {
 
-	ModelManager::GetInstance()->LoadModel("Resources/3d","HitEffect");
-	
+	ModelManager::GetInstance()->LoadModel("Resources/3d", "HitEffect");
+
 	hitEffect_ = std::make_unique<Object3d>();
 	hitEffect_->Initialize();
 	hitEffect_->SetModel("HitEffect");
@@ -17,27 +22,34 @@ void EnemyHitEffect::Initialize() {
         .rotate{0, 0, 0},
         .translate{0, 0, 0}
     };
-	hitPrimitive_ = std::make_unique<Primitive>();
-	hitPrimitive_->Initialize(Primitive::Plane, "Resources/3d/Circle.png");
-	hitPrimitiveTransform_ = {
-	    .scale{3.0f, 0.001f, 1.0f},
-        .rotate{0, 0, 0},
-        .translate{0, 0, 0}
-    };
-	hitPrimitive_->SetTransform(hitPrimitiveTransform_);
-	hitPrimitive_->SetEnableLighting(false);
-	hitPrimitive_->SetCamera(camera_);
 
+	constexpr float kBaseRotations[kParticleCount] = {
+	    0.0f, 0.42f, 0.96f, 1.58f, 2.24f, 2.86f, 3.44f,
+	};
+	constexpr Vector3 kBaseScales[kParticleCount] = {
+	    {1.15f, 1.15f, 1.0f},
+        {0.82f, 0.82f, 1.0f},
+        {1.42f, 1.42f, 1.0f},
+        {0.64f, 0.64f, 1.0f},
+        {1.0f,  1.0f,  1.0f},
+        {1.28f, 1.28f, 1.0f},
+        {0.74f, 0.74f, 1.0f},
+	};
 
-	hitPrimitiveInner_ = std::make_unique<Primitive>();
-	hitPrimitiveInnerTransform_ = {
-	    .scale{0.001f, 3.0f, 1.0f},
-        .rotate{0,    0,    0   },
-        .translate{0,    0,    0   }
-    };
-	hitPrimitiveInner_->Initialize(Primitive::Plane, "Resources/3d/Circle.png");
-	hitPrimitiveInner_->SetEnableLighting(false);
-	hitPrimitiveInner_->SetTransform(hitPrimitiveInnerTransform_);
+	for (std::size_t i = 0; i < kParticleCount; ++i) {
+		particleBaseRotations_[i] = kBaseRotations[i];
+		particleBaseScales_[i] = kBaseScales[i];
+		hitParticleTransforms_[i] = {
+		    .scale{kBaseScales[i]},
+            .rotate{0.0f, 0.0f, kBaseRotations[i]},
+            .translate{0.0f, 0.0f, 0.0f}
+        };
+		hitParticles_[i] = std::make_unique<Primitive>();
+		hitParticles_[i]->Initialize(Primitive::Plane, kParticleTexturePath);
+		hitParticles_[i]->SetTransform(hitParticleTransforms_[i]);
+		hitParticles_[i]->SetEnableLighting(false);
+		hitParticles_[i]->SetCamera(camera_);
+	}
 
 	enemyPosition_ = {0.0f, 0.0f, 0.0f};
 }
@@ -53,35 +65,39 @@ void EnemyHitEffect::Update() {
 		return;
 	}
 
-	activeTimer_ -= 1.0f / 60.0f;
+	activeTimer_ -= kFrameTime;
 	if (activeTimer_ <= 0.0f) {
 		isActive_ = false;
 		return;
 	}
 
+	const float progress = 1.0f - (activeTimer_ / activeDuration_);
+	const float popScale = 0.75f + progress * 1.65f;
+	const float fade = activeTimer_ / activeDuration_;
+
 	hitTransform_.translate = enemyPosition_;
 	hitTransform_.rotate.y = std::numbers::pi_v<float>;
-	hitPrimitiveTransform_.translate = enemyPosition_;
-	hitPrimitiveTransform_.rotate.y = std::numbers::pi_v<float>;
-	hitPrimitiveInnerTransform_.translate = enemyPosition_;
-	hitPrimitiveInnerTransform_.rotate.y = std::numbers::pi_v<float>;
 
 	Matrix4x4 c = camera_->GetWorldMatrix();
 	c.m[3][0] = c.m[3][1] = c.m[3][2] = 0;
 	Matrix4x4 world = Function::Multiply(c, Function::MakeAffineMatrix(hitTransform_.scale, hitTransform_.rotate, hitTransform_.translate));
-	Matrix4x4 primitiveWorld = Function::Multiply(c, Function::MakeAffineMatrix(hitPrimitiveTransform_.scale, hitPrimitiveTransform_.rotate, hitPrimitiveTransform_.translate));
-	Matrix4x4 primitiveInnerWorld = Function::Multiply(c, Function::MakeAffineMatrix(hitPrimitiveInnerTransform_.scale, hitPrimitiveInnerTransform_.rotate, hitPrimitiveInnerTransform_.translate));
-	
+
 	hitEffect_->SetCamera(camera_);
 	hitEffect_->SetWorldMatrix(world);
 	hitEffect_->Update();
-	hitPrimitive_->SetCamera(camera_);
-	hitPrimitive_->SetWorldMatrix(primitiveWorld);
-	hitPrimitive_->Update();
-	hitPrimitiveInner_->SetCamera(camera_);
-	hitPrimitiveInner_->SetWorldMatrix(primitiveInnerWorld);
-	hitPrimitiveInner_->Update();
-	
+
+	for (std::size_t i = 0; i < kParticleCount; ++i) {
+		hitParticleTransforms_[i].translate = enemyPosition_;
+		hitParticleTransforms_[i].rotate.y = std::numbers::pi_v<float>;
+		hitParticleTransforms_[i].rotate.z = particleBaseRotations_[i] + progress * (1.8f + 0.22f * static_cast<float>(i));
+		hitParticleTransforms_[i].scale = {particleBaseScales_[i].x * popScale, particleBaseScales_[i].y * popScale, particleBaseScales_[i].z};
+
+		Matrix4x4 particleWorld = Function::Multiply(c, Function::MakeAffineMatrix(hitParticleTransforms_[i].scale, hitParticleTransforms_[i].rotate, hitParticleTransforms_[i].translate));
+		hitParticles_[i]->SetCamera(camera_);
+		hitParticles_[i]->SetWorldMatrix(particleWorld);
+		hitParticles_[i]->SetColor({1.0f, 0.72f, 0.24f, fade});
+		hitParticles_[i]->Update();
+	}
 }
 
 void EnemyHitEffect::Draw() {
@@ -90,6 +106,7 @@ void EnemyHitEffect::Draw() {
 	}
 
 	hitEffect_->Draw();
-	//hitPrimitive_->Draw();
-	//hitPrimitiveInner_->Draw();
+	for (auto& particle : hitParticles_) {
+		particle->Draw();
+	}
 }
--- project/application/Object/Characters/Enemy/EnemyHitEffect.h
+++ project/application/Object/Characters/Enemy/EnemyHitEffect.h
@@ -1,23 +1,27 @@
 #pragma once
-#include <memory>
 #include "Object3d/Object3d.h"
-#include <vector>
-#include "Transform.h"
 #include "Primitive/Primitive.h"
+#include "Transform.h"
 #include "Vector3.h"
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <vector>
 class EnemyHitEffect {
 
+	static constexpr std::size_t kParticleCount = 7;
+
 	std::unique_ptr<Object3d> hitEffect_;
-	std::unique_ptr<Primitive> hitPrimitive_;
-	std::unique_ptr<Primitive> hitPrimitiveInner_;
+	std::array<std::unique_ptr<Primitive>, kParticleCount> hitParticles_;
+	std::array<Transform, kParticleCount> hitParticleTransforms_;
+	std::array<float, kParticleCount> particleBaseRotations_{};
+	std::array<Vector3, kParticleCount> particleBaseScales_{};
 	Transform hitTransform_;
-	Transform hitPrimitiveTransform_;
-	Transform hitPrimitiveInnerTransform_;
 	Camera* camera_ = nullptr;
 	Vector3 enemyPosition_;
 	bool isActive_ = false;
 	float activeTimer_ = 0.0f;
-	float activeDuration_ = 0.2f;
+	float activeDuration_ = 0.28f;
 
 public:
 	void Initialize();
@@ -27,6 +31,4 @@ public:
 	bool IsActive() const { return isActive_; }
 	void Update();
 	void Draw();
-
-
 };
