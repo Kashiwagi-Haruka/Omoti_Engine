4e02a492061fadd1972f53a3cf18799d3bfb54d8
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-06-17 16:55:36 +0900

    a

--- project/application/UI/Menu/Menu.cpp
+++ project/application/UI/Menu/Menu.cpp
@@ -1,13 +1,45 @@
 #include "Menu.h"
 #include "TextureManager.h"
+
+namespace{
+constexpr Vector2 kIconMargin = { 20.0f, 20.0f };
+constexpr Vector2 kIconSize = {100.0f, 100.0f};
+
+constexpr Vector2 kKeyIconSize = {100.0f, 40.0f};
+}
+
 Menu::Menu() { 
 	pauseSprite_ = std::make_unique<Sprite>();
 	characterDisplaySprite_ = std::make_unique<Sprite>();
 	teamSelectSprite_ = std::make_unique<Sprite>();
+	helpSprite_ = std::make_unique<Sprite>();
+
+	pauseKeyboardSprite_ = std::make_unique<Sprite>();
+	characterDisplayKeyboardSprite_ = std::make_unique<Sprite>();
+	teamSelectKeyboardSprite_ = std::make_unique<Sprite>();
+	helpKeyboardSprite_ = std::make_unique<Sprite>();
 }
 Menu::~Menu() {}
-void Menu::Initialize() {
-	
+void Menu::Initialize() { 
+	pauseSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Pause.png")); 
+	characterDisplaySprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/CharacterDisplay.png"));
+	teamSelectSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Team.png"));
+	helpSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Help.png"));
+
+	pauseKeyboardSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Keyboard/PauseKey.png"));
+	characterDisplayKeyboardSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Keyboard/CharacterDisplayKey.png"));
+	teamSelectKeyboardSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Keyboard/TeamKey.png"));
+	helpKeyboardSprite_->Initialize(TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Menu/Keyboard/HelpKey.png"));
+
+	pauseSprite_->SetScale(kIconSize);
+	characterDisplaySprite_->SetScale(kIconSize);
+	teamSelectSprite_->SetScale(kIconSize);
+	helpSprite_->SetScale(kIconSize);
+
+	pauseKeyboardSprite_->SetScale(kKeyIconSize);
+	characterDisplayKeyboardSprite_->SetScale(kKeyIconSize);
+	teamSelectKeyboardSprite_->SetScale(kKeyIconSize);
+	helpKeyboardSprite_->SetScale(kKeyIconSize);
 }
 void Menu::Update() {}
 void Menu::Draw() {}
