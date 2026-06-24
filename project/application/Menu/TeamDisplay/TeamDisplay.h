ab6909b340bf11a403c0276574b8d7fa8a6dabde
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-06-24 14:52:57 +0900

    a

---
+++ project/application/Menu/TeamDisplay/TeamDisplay.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Object/Characters/Playable/Base/PlayableBase.h"
+#include "Primitive/Primitive.h"
+#include "Sprite/Sprite.h"
+#include "Text/Text.h"
+#include <memory>
+class TeamDisplay {
+
+public:
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+
+
+
+};
