7cd775fc042d79818d006c8f70004e7f88b0dc3b
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-06-24 14:56:48 +0900

    a

---
+++ project/application/Object/Characters/Enemy/Base/BaseEnemy.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Object/Characters/Base/CharacterParameters.h"
+#include "Transform.h"
+#include "Vector3.h"
+#include <memory>
+#include <string>
+
+class Camera;
+class Object3d;
+
+class BaseEnemy {
+protected:
+	BaseParameter baseParameter_{10.0f, 0.0f, 10.0f};
+	Parameter parameter_{};
+	Transform transform_{};
+	Vector3 velocity_{};
+	std::unique_ptr<Object3d> object_;
+	Camera* camera_ = nullptr;
+	bool isAlive_ = true;
+	std::string modelName_ = "Enemy";
+
+public:
+	BaseEnemy();
+	virtual ~BaseEnemy();
+
+	virtual void Initialize(Camera* camera, const Vector3& translate);
+	virtual void Update();
+	virtual void Draw();
+
+	void SetCamera(Camera* camera);
+	void SetPosition(const Vector3& position);
+	void SetVelocity(const Vector3& velocity) { velocity_ = velocity; }
+	void SetModelName(const std::string& modelName) { modelName_ = modelName; }
+	void SetBaseParameter(const BaseParameter& baseParameter) { baseParameter_ = baseParameter; }
+	void SetParameter(const Parameter& parameter) { parameter_ = parameter; }
+	void SetIsAlive(bool isAlive) { isAlive_ = isAlive; }
+
+	const BaseParameter& GetBaseParameter() const { return baseParameter_; }
+	const Parameter& GetParameter() const { return parameter_; }
+	const Transform& GetTransform() const { return transform_; }
+	Vector3 GetPosition() const { return transform_.translate; }
+	Vector3 GetScale() const { return transform_.scale; }
+	bool GetIsAlive() const { return isAlive_; }
+};
--- project/application/Object/Characters/Enemy/Individual/brain/brain.h
+++ project/application/Object/Characters/Enemy/Individual/brain/brain.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "Object/Characters/Enemy/BaseEnemy.h"
-class brain : public BaseEnemy{
 
+class brain : public BaseEnemy {
+public:
+	brain();
+	~brain() override = default;
 
-
-
+	void Initialize(Camera* camera, const Vector3& translate) override;
+	void Update() override;
 };
