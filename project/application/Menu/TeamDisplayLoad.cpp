3a1d1b4256eb4a7cbb6e6440dfd30a12584d39ec
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-06-24 15:23:53 +0900

    a

---
+++ project/application/Menu/TeamDisplayLoad.cpp
@@ -0,0 +1,1 @@
+#include "TeamDisplayLoad.h"
---
+++ project/application/Menu/TeamDisplayLoad.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Text/Text.h"
+#include "Sprite/Sprite.h"
+class TeamDisplayLoad {
+
+
+
+
+
+public:
+
+	TeamDisplayLoad();
+	~TeamDisplayLoad();
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+
+};
