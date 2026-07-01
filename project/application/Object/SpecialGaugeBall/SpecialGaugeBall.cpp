a130f70ce709323d0d63f91164997d0b2edcc8ab
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-01 16:16:53 +0900

    a

--- project/application/Object/SpecialGaugeBall/SpecialGaugeBall.cpp
+++ project/application/Object/SpecialGaugeBall/SpecialGaugeBall.cpp
@@ -24,7 +24,7 @@ void SpecialGaugeBall::Initialize(Camera* camera, const Vector3& position) {
 	};
 	for (int i = 0; i < 3; i++) {
 		primitive_[i] = std::make_unique<Primitive>();
-		primitive_[i]->Initialize(Primitive::Box, "Resources/2d/ExpGauge.png");
+		primitive_[i]->Initialize(Primitive::Box, "Resources/2d/defaultParticle.png");
 		primitive_[i]->SetEnableLighting(false);
 		primitive_[i]->SetColor(kExpCubeColor);
 		primitive_[i]->SetTransform(baseTransform_);
