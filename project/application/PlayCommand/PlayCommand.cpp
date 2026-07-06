1c60c10a81e53a784cb47e9ec82d9d8bc01a473c
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-07 03:45:44 +0900

    a

--- project/application/PlayCommand/PlayCommand.cpp
+++ project/application/PlayCommand/PlayCommand.cpp
@@ -32,7 +32,7 @@ bool PlayCommand::GetJUMP() {
 	return false;
 }
 bool PlayCommand::GetDASH() {
-	if (Input::GetInstance()->PushMouseButton(Input::MouseButton::kRight) || Input::GetInstance()->PushButton(Input::PadButton::kButtonLeftShoulder)) {
+	if (Input::GetInstance()->PushMouseButton(Input::MouseButton::kRight) || Input::GetInstance()->PushButton(Input::PadButton::kButtonRightThumb)) {
 		return true;
 	}
 	return false;
