7e1fc840a20ae91c876f734d1db855c257417b81
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-07 12:43:23 +0900

    a

--- project/application/CameraController/NormalAttackCamera/NormalAttackCamera.cpp
+++ project/application/CameraController/NormalAttackCamera/NormalAttackCamera.cpp
@@ -1,1 +1,76 @@
 #include "NormalAttackCamera.h"
+#include "Camera.h"
+#include "Function.h"
+#include "Input.h"
+#include <cmath>
+#include <imgui.h>
+#include <numbers>
+#include <random>
+
+namespace {
+
+float NormalizeAngle(float angle) {
+	const float twoPi = 2.0f * std::numbers::pi_v<float>;
+	while (angle > std::numbers::pi_v<float>) {
+		angle -= twoPi;
+	}
+	while (angle < -std::numbers::pi_v<float>) {
+		angle += twoPi;
+	}
+	return angle;
+}
+
+float GetNearestAngle(float current, float target) { return current + NormalizeAngle(target - current); }
+
+} // namespace
+
+NormalAttackCamera::NormalAttackCamera() {}
+NormalAttackCamera::~NormalAttackCamera() {}
+void NormalAttackCamera::Initialize() {
+
+	transform_ = {
+	    .scale{1.0f,        1.0f,      1.0f  },
+        .rotate{orbitPitch_, orbitYaw_, 0.0f  },
+        .translate{0.0f,        10.0f,     -50.0f}
+    };
+
+	camera_ = std::make_unique<Camera>();
+	camera_->SetTransform(transform_);
+}
+void NormalAttackCamera::Update() {
+
+	const float maxPitch = 1.2f;
+	const float minPitch = -1.2f;
+	if (orbitPitch_ > maxPitch) {
+		orbitPitch_ = maxPitch;
+	}
+	if (orbitPitch_ < minPitch) {
+		orbitPitch_ = minPitch;
+	}
+	if (!hasTarget_ && hasFollowPosition_) {
+		Vector3 toFollow = followPosition_ - playerPos;
+		if (Function::LengthSquared(toFollow) > 0.0001f) {
+			orbitYaw_ = GetNearestAngle(orbitYaw_, std::atan2f(toFollow.x, toFollow.z));
+		}
+	}
+	Vector3 orbitDir = {sinf(orbitYaw_) * cosf(orbitPitch_), -sinf(orbitPitch_), cosf(orbitYaw_) * cosf(orbitPitch_)};
+
+	transform_.translate = playerPos - orbitDir * distance_;
+	transform_.rotate = {orbitPitch_, orbitYaw_, 0.0f};
+
+	if (shakeTimer_ > 0.0f) {
+		shakeTimer_ -= 1.0f / 60.0f;
+		if (shakeTimer_ < 0.0f) {
+			shakeTimer_ = 0.0f;
+		}
+		const float decay = shakeDuration_ > 0.0f ? (shakeTimer_ / shakeDuration_) : 0.0f;
+		const float strength = shakeAmplitude_ * decay;
+		static std::mt19937 rng{std::random_device{}()};
+		std::uniform_real_distribution<float> offsetDist(-1.0f, 1.0f);
+		transform_.translate.x += offsetDist(rng) * strength;
+		transform_.translate.y += offsetDist(rng) * strength;
+	}
+
+	camera_->SetTransform(transform_);
+	camera_->Update();
+}
--- project/application/UI/Team/TeamUI.cpp
+++ project/application/UI/Team/TeamUI.cpp
@@ -1,24 +1,33 @@
 #include "TeamUI.h"
 #include "Function.h"
+#include "SpriteCommon.h"
 #include "Text/FreeTypeManager/FreeTypeManager.h"
 #include "TextureManager.h"
-#include "SpriteCommon.h"
 #include "WinApp.h"
 namespace {
 constexpr Vector2 kIconSize{35.0f, 35.0f};
 constexpr Vector2 kActiveIconSize{40.0f, 40.0f};
+constexpr Vector2 kPadIconSize{28.0f, 28.0f};
+constexpr float kPadIconGap = 8.0f;
 constexpr Vector2 kHpBarMaxSize{30.0f, 4.0f};
 constexpr Vector2 kHpBarOffset{-kIconSize.x * 1.3f, kIconSize.y * 0.9f};
 constexpr Vector2 kCharacterNameOffset{-kIconSize.x * 1.3f, 16.0f};
 constexpr Vector2 kCharacterNameTextSize{120.0f, 24.0f};
-constexpr float kRightMargin = 20.0f;
+constexpr float kRightMargin = 20.0f + kPadIconSize.x + kPadIconGap;
 constexpr float kTopMargin = 160.0f;
 constexpr float kIconSpacing = 14.0f;
 constexpr Vector2 kRightTopAnchor{1.0f, 0.0f};
+constexpr Vector2 kLeftTopAnchor{0.0f, 0.0f};
 constexpr Vector4 kActiveColor{1.0f, 1.0f, 1.0f, 1.0f};
 constexpr Vector4 kInactiveColor{0.65f, 0.65f, 0.65f, 0.78f};
 constexpr Vector4 kCharacterNameColor{1.0f, 1.0f, 1.0f, 1.0f};
 constexpr Vector4 kInactiveCharacterNameColor{0.75f, 0.75f, 0.75f, 0.78f};
+constexpr std::array<const char*, Team::kMaxMembersCount> kPadTexturePaths{
+    "Resources/2d/Team/UI/Pad/UP.png",
+    "Resources/2d/Team/UI/Pad/RIGHT.png",
+    "Resources/2d/Team/UI/Pad/DOWN.png",
+    "Resources/2d/Team/UI/Pad/LEFT.png",
+};
 } // namespace
 
 TeamUI::TeamUI() { displayedCharacterIndices_.fill(-1); }
@@ -31,6 +40,10 @@ void TeamUI::Initialize(const Team& team) {
 	uint32_t specialGaugeTextureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/AttackOperation/specialGauge.png");
 	uint32_t specialGaugeFlameTextureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/AttackOperation/special.png");
 	uint32_t selectedBackgroundTextureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/Team/UI/SelectedBackground.png");
+	std::array<uint32_t, kMaxMembersCount> padTextureHandles{};
+	for (int i = 0; i < kMaxMembersCount; ++i) {
+		padTextureHandles[i] = TextureManager::GetInstance()->GetTextureIndexByfilePath(kPadTexturePaths[i]);
+	}
 
 	characterNameFontHandle_ = FreeTypeManager::CreateFace("Resources/Font/irohakakuC-Medium.ttf", 0);
 	FreeTypeManager::SetPixelSizes(characterNameFontHandle_, 14, 14);
@@ -53,6 +66,7 @@ void TeamUI::Initialize(const Team& team) {
 			iconSprites_[i].reset();
 			hpBarSprites_[i].reset();
 			hpBarBackgroundSprites_[i].reset();
+			padSprites_[i].reset();
 			specialGaugeFlameSprites_[i].reset();
 			specialGaugeSprites_[i].reset();
 			continue;
@@ -65,6 +79,14 @@ void TeamUI::Initialize(const Team& team) {
 		iconSprites_[i]->SetScale(i == team.GetActiveSlot() ? kActiveIconSize : kIconSize);
 		iconSprites_[i]->SetColor(i == team.GetActiveSlot() ? kActiveColor : kInactiveColor);
 		iconSprites_[i]->Update();
+
+		padSprites_[i] = std::make_unique<Sprite>();
+		padSprites_[i]->Initialize(padTextureHandles[i]);
+		padSprites_[i]->SetAnchorPoint(kLeftTopAnchor);
+		padSprites_[i]->SetPosition(iconPositions_[i] + Vector2{kPadIconGap, 0.0f});
+		padSprites_[i]->SetScale(kPadIconSize);
+		padSprites_[i]->SetColor(i == team.GetActiveSlot() ? kActiveColor : kInactiveColor);
+		padSprites_[i]->Update();
 
 		hpBarSprites_[i] = std::make_unique<Sprite>();
 		hpBarSprites_[i]->Initialize(hpBarTextureHandle);
@@ -98,8 +120,6 @@ void TeamUI::Initialize(const Team& team) {
 		characterNameTexts_[i].SetPosition(iconPositions_[i] + kCharacterNameOffset);
 		characterNameTexts_[i].SetColor(i == team.GetActiveSlot() ? kCharacterNameColor : kInactiveCharacterNameColor);
 		characterNameTexts_[i].UpdateLayout(false);
-
-		
 	}
 
 	selectedBackgroundSprite_ = std::make_unique<Sprite>();
@@ -107,6 +127,7 @@ void TeamUI::Initialize(const Team& team) {
 	selectedBackgroundSprite_->SetAnchorPoint({1.0f, 0.0f});
 	selectedBackgroundSprite_->SetScale({kActiveIconSize.x + kHpBarMaxSize.x + (-kHpBarOffset.x), kActiveIconSize.y});
 	selectedBackgroundSprite_->SetPosition(iconPositions_[team.GetActiveSlot()]);
+	selectedBackgroundSprite_->SetColor({0.4f, 0.4f, 0.5f, 0.7f});
 	selectedBackgroundSprite_->Update();
 }
 
@@ -119,6 +140,7 @@ void TeamUI::Update(const Team& team) {
 			iconSprites_[i].reset();
 			hpBarSprites_[i].reset();
 			hpBarBackgroundSprites_[i].reset();
+			padSprites_[i].reset();
 			specialGaugeFlameSprites_[i].reset();
 			specialGaugeSprites_[i].reset();
 			displayedCharacterIndices_[i] = -1;
@@ -132,6 +154,13 @@ void TeamUI::Update(const Team& team) {
 		} else if (displayedCharacterIndices_[i] != characterIndex) {
 			iconSprites_[i]->Initialize(team.GetOwnedCharacterIconHandle(characterIndex));
 			iconSprites_[i]->SetAnchorPoint(kRightTopAnchor);
+		}
+
+		if (!padSprites_[i]) {
+			const uint32_t padTextureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath(kPadTexturePaths[i]);
+			padSprites_[i] = std::make_unique<Sprite>();
+			padSprites_[i]->Initialize(padTextureHandle);
+			padSprites_[i]->SetAnchorPoint(kLeftTopAnchor);
 		}
 
 		if (!hpBarSprites_[i]) {
@@ -165,6 +194,11 @@ void TeamUI::Update(const Team& team) {
 		iconSprites_[i]->SetColor(i == team.GetActiveSlot() ? kActiveColor : kInactiveColor);
 		iconSprites_[i]->Update();
 
+		padSprites_[i]->SetPosition(iconPositions_[i] + Vector2{kPadIconGap, 0.0f});
+		padSprites_[i]->SetScale(kPadIconSize);
+		padSprites_[i]->SetColor(i == team.GetActiveSlot() ? kInactiveColor : kActiveColor);
+		padSprites_[i]->Update();
+
 		hpBarSprites_[i]->SetPosition(iconPositions_[i] + kHpBarOffset);
 		hpBarSprites_[i]->SetScale(kHpBarMaxSize);
 		hpBarSprites_[i]->Update();
@@ -197,6 +231,11 @@ void TeamUI::Draw() {
 	for (auto& iconSprite : iconSprites_) {
 		if (iconSprite) {
 			iconSprite->Draw();
+		}
+	}
+	for (auto& padSprite : padSprites_) {
+		if (padSprite) {
+			padSprite->Draw();
 		}
 	}
 	for (auto& characterNameText : characterNameTexts_) {
