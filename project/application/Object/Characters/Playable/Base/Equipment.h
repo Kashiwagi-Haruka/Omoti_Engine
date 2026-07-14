c01cd8a417ec5e51f6e42676fedfdf7189eaa8db
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-14 15:23:28 +0900

    a

--- project/application/Object/Characters/Playable/Base/Equipment.h
+++ project/application/Object/Characters/Playable/Base/Equipment.h
@@ -1,9 +1,31 @@
-enum EquipmentTypes{
+enum class EquipmentTypes{
 	HEAD,//頭
 	NECK,//首
 	CHEST,//胸
 	PANTS,//ズボン
 	SHOES,//靴
+};
+
+enum class EquipUpParametersType{ 
+	//HP実数値
+	HPRealValue,
+	//HPパーセント
+	HPPercent,
+	//攻撃実数値
+	AttackRealValue,
+	//攻撃パーセント
+	AttackPercent,
+	//防御実数値
+	DiffenceRealValue,
+	//防御パーセント
+	DiffencePercent,
+	// クリティカル率
+	CriticalRate,
+	// クリティカルダメージ
+	CriticalDamage,
+	// 属性親和度
+	AttributeAffinity,
+
 };
 class Equipment{
 	EquipmentTypes equipmentType_;
