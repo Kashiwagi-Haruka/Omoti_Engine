baae205d44cfad262eb650a8a04185857f382d63
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-14 12:58:41 +0900

    a

---
+++ project/application/Object/Characters/Enemy/LockOn/LockOnEnemy.cpp
@@ -0,0 +1,1 @@
+#include "LockOnEnemy.h"
---
+++ project/application/Object/Characters/Enemy/LockOn/LockOnEnemy.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Primitive/Primitive.h"
+#include <memory>
+class LockOnEnemy {
+
+	std::unique_ptr<Primitive> lockOnPrimitive_;
+
+
+
+
+
+};
