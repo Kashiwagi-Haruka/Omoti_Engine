e774e735b293233973f999d6f3843f13efaa3fa6
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-15 15:02:57 +0900

    a

--- project/application/Menu/CharacterDisplay/Menu/Skilltree/CharacterDisplaySkilltree.cpp
+++ project/application/Menu/CharacterDisplay/Menu/Skilltree/CharacterDisplaySkilltree.cpp
@@ -1,6 +1,7 @@
 #include "CharacterDisplaySkilltree.h"
 #include "TextureManager.h"
 #include "ScreenSize.h"
+#include <numbers>
 namespace{
 float skillTreeIconSize = 40.0f;
 float skillTreeTextureSize = 200.0f;
@@ -23,6 +24,8 @@ CharacterDisplaySkilltree::CharacterDisplaySkilltree() {
 void CharacterDisplaySkilltree::Initialize() { 
 	std::string textureName = "Resources/2d/CharacterDIsplay/skillTree/character/" + selectCharacterName_ + ".png";
 	uint32_t textureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath(textureName);
+	uint32_t parameterTextureHandle = TextureManager::GetInstance()->GetTextureIndexByfilePath("Resources/2d/CharacterDIsplay/skillTree/parameter.png");
+	                                     
 
 	normalAttackSprite_->Initialize(textureHandle);
 	normalAttackSprite_->SetScale({skillTreeIconSize, skillTreeIconSize});
@@ -43,6 +46,15 @@ void CharacterDisplaySkilltree::Initialize() {
 	AddAbilityRightSprite_->Initialize(textureHandle);
 	AddAbilityRightSprite_->SetScale({skillTreeIconSize, skillTreeIconSize});
 	AddAbilityRightSprite_->SetTextureRange({skillTreeTextureSize * 4.0f, 0.0f}, {skillTreeTextureSize, skillTreeTextureSize});
+	
+	for (int i = 0; i < kMaxskillTreeCount_; i++) {
+		leftSkilltreeSprites_[i]->Initialize(parameterTextureHandle);
+		leftSkilltreeSprites_[i]->SetScale({skillTreeIconSize, skillTreeIconSize});
+		leftSkilltreeSprites_[i]->SetTextureRange({0.0f, 0.0f}, {skillTreeTextureSize, skillTreeTextureSize});
+		rightSkilltreeSprites_[i]->Initialize(parameterTextureHandle);
+		rightSkilltreeSprites_[i]->SetScale({skillTreeIconSize, skillTreeIconSize});
+		rightSkilltreeSprites_[i]->SetTextureRange({0.0f, 0.0f}, {skillTreeTextureSize, skillTreeTextureSize});
+	}
 }
 void CharacterDisplaySkilltree::Update() {
 
@@ -53,6 +65,12 @@ void CharacterDisplaySkilltree::Update() {
 		skillAttackSprite_->SetPosition({AddAbilityLeftSprite_->GetPosition().x-60.0f, AddAbilityLeftSprite_->GetPosition().y-60.0f});
 		normalAttackSprite_->SetPosition({skillAttackSprite_->GetPosition().x - 60.0f, skillAttackSprite_->GetPosition().y - 60.0f});
 		specialAttackSprite_->SetPosition({AddAbilityRightSprite_->GetPosition().x+60.0f, AddAbilityRightSprite_->GetPosition().y-60.0f});
+		for (int i = 0; i < kMaxskillTreeCount_; i++) {
+			leftSkilltreeSprites_[i]->SetPosition(
+			    {AddAbilityLeftSprite_->GetPosition().x - sinf(std::numbers::pi_v<float> * ((i + 1.0f) / 6)), AddAbilityLeftSprite_->GetPosition().y + (20.0f * (i + 1))});
+			rightSkilltreeSprites_[i]->SetPosition(
+			    {AddAbilityLeftSprite_->GetPosition().x + sinf(std::numbers::pi_v<float> * ((i + 1.0f) / 6)), AddAbilityLeftSprite_->GetPosition().y + (20.0f * (i + 1))});
+		}
 		break;
 	case Arcana::Magician:
 		break;
@@ -104,6 +122,10 @@ void CharacterDisplaySkilltree::Update() {
 	specialAttackSprite_->Update();
 	AddAbilityLeftSprite_->Update();
 	AddAbilityRightSprite_->Update();
+	for (int i = 0; i < kMaxskillTreeCount_; i++) {
+		leftSkilltreeSprites_[i]->Update();
+		rightSkilltreeSprites_[i]->Update();
+	}
 }
 void CharacterDisplaySkilltree::Draw() {
 
@@ -112,6 +134,10 @@ void CharacterDisplaySkilltree::Draw() {
 	specialAttackSprite_->Draw();
 	AddAbilityLeftSprite_->Draw();
 	AddAbilityRightSprite_->Draw();
+	for (int i = 0; i < kMaxskillTreeCount_; i++) {
+		leftSkilltreeSprites_[i]->Draw();
+		rightSkilltreeSprites_[i]->Draw();
+	}
 }
 
 void CharacterDisplaySkilltree::SetCharacterName(std::string name) { selectCharacterName_ = name; }
