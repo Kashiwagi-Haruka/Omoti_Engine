00c6d3ee16b9cd3313770730bd71a0e53e3e7a86
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-23 15:40:32 +0900

    a

--- project/application/Object/Characters/Playable/Individual/Sizuku/SizukuSpecial.h
+++ project/application/Object/Characters/Playable/Individual/Sizuku/SizukuSpecial.h
@@ -10,7 +10,7 @@ class SizukuSpecial{
 	bool isStarted_ = false; // 開始フラグ
 	bool isEnd_ = false;
 	float duration_ = 0;     // 継続時間
-	float durationMax_ = 15; // 最大継続時間(秒)
+	float durationMax_ = 1; // 最大継続時間(秒)
 	float sizukuHeight_ = 0; // 雫の高さ
 	float rainInterval_ = 0.5f; // 雨の降る間隔(秒)
 	float rainTimer_ = 0;       // 雨のタイマー
