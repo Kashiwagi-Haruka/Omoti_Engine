ee9edfa3e5939ec9a64fd8f826203ea49e057252
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-07-24 15:07:37 +0900

    a

--- project/application/Menu/CharacterDisplay/Menu/Equip/CharacterDisplayEquip.cpp
+++ project/application/Menu/CharacterDisplay/Menu/Equip/CharacterDisplayEquip.cpp
@@ -1,5 +1,29 @@
 #include "CharacterDisplayEquip.h"
+#include "Function.h"
+#include "Object3d/Object3dCommon.h"
+#include <cmath>
+#include <numbers>
+void CharacterDisplayEquip::Initialize() { 
 
-void CharacterDisplayEquip::Initialize() {}
-void CharacterDisplayEquip::Update() {}
-void CharacterDisplayEquip::Draw() {}
+	for (int i = 0; i < 5; i++) {
+		viewEquipment_[i] = std::make_unique<Primitive>();
+		viewEquipment_[i]->Initialize(Primitive::PrimitiveName::Circle);
+		viewEquipmentTransform_[i]->translate = {std::sinf(std::numbers::pi_v<float> * (1.0f / 5.0f * i)) * distance_, 0.0f, std::cosf(std::numbers::pi_v<float> * (1.0f / 5.0f * i)) * distance_};
+	}
+
+
+
+}
+void CharacterDisplayEquip::Update() {
+	for (int i = 0; i < 5; i++) {
+		viewEquipment_[i]->SetTranslate(centerTranslate_);
+		viewEquipment_[i]->Update();
+	}
+
+}
+void CharacterDisplayEquip::Draw() {
+	Object3dCommon::GetInstance()->DrawCommon();
+	for (int i = 0; i < 5; i++) {
+		viewEquipment_[i]->Draw();
+	}
+}
--- project/application/Menu/CharacterDisplay/Menu/Equip/CharacterDisplayEquip.h
+++ project/application/Menu/CharacterDisplay/Menu/Equip/CharacterDisplayEquip.h
@@ -3,11 +3,16 @@
 #include <memory>
 #include "Primitive/Primitive.h"
 class CharacterDisplayEquip {
-	
-	std::array<std::unique_ptr<Primitive>, 5> viewWquipment_;
+
+	std::array<std::unique_ptr<Primitive>, 5> viewEquipment_;
+	std::array<std::unique_ptr<Transform>, 5> viewEquipmentTransform_;
+	float distance_ = 2.0f; //中心からの距離
+	Vector3 centerTranslate_;
+
 
 public:
 	void Initialize();
 	void Update();
 	void Draw();
+	void SetTranslate(Vector3 centerTranslate) { centerTranslate_ = centerTranslate; };
 };
